58c768743b8ba13ee4481f41a576ac13a2737d79
Muhammad Firzak Rizki <firzakrizki@gmail.com>
2023-11-15 18:48:36 +0700

    post

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -12,7 +14,7 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() : Response
+    public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
             //
@@ -30,9 +32,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
